e75edfac3f13ad818ce179dc8809e3eaf44cc67a
eduvadillo <87086693+eduvadillo@users.noreply.github.com>
2021-12-10 10:11:47 +0100

    try back and forward

--- routes/index.js
+++ routes/index.js
@@ -39,4 +39,35 @@ router.get("/version", (req, res, next) => {
   );
 });
 
+router.post("/version-back", (req, res, next) => {
+  const { versionLess } = req.body;
+
+  console.log(`soy el version less`, versionLess);
+
+  Version.find({ _id: ["61b1cc172b34fc1a17101e5c"] }).then((web) =>
+    web[0].version === 1
+      ? Version.findOneAndUpdate({ _id: ["61b1cc172b34fc1a17101e5c"] }, { version: 3 })
+          .then((web) => {
+            res.json(web);
+          })
+
+          .catch((err) => res.json(err))
+      : web[0].version === 2
+      ? Version.findOneAndUpdate({ _id: ["61b1cc172b34fc1a17101e5c"] }, { version: 1 })
+          .then((web) => {
+            res.json(web);
+          })
+
+          .catch((err) => res.json(err))
+      : web[0].version === 3
+      ? Version.findOneAndUpdate({ _id: ["61b1cc172b34fc1a17101e5c"] }, { version: 2 })
+          .then((web) => {
+            res.json(web);
+          })
+
+          .catch((err) => res.json(err))
+      : res.json(`hola`)
+  );
+});
+
 module.exports = router;
