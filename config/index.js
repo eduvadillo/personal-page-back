bedfc800e8acf31c4f41d8acc6a805e0cb16e5ec
eduvadillo <87086693+eduvadillo@users.noreply.github.com>
2021-12-10 09:12:43 +0100

    try heroku

--- config/index.js
+++ config/index.js
@@ -20,12 +20,8 @@ module.exports = (app) => {
   app.set("trust proxy", 1);
 
   // controls a very specific header to pass headers from the frontend
-  app.use(
-    cors({
-      credentials: true,
-      origin: process.env.ORIGIN || "http://localhost:3000",
-    })
-  );
+
+  app.use(cors());
 
   // In development environment the app logs
   app.use(logger("dev"));
